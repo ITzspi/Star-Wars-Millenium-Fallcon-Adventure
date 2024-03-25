ff1f65363eaa2d4c4d5b1facfa3985ed25c7cda8
Felipe Avelino Pedaes <141787351+ITzspi@users.noreply.github.com>
2024-03-24 23:05:56 -0300

    Add files via upload

---
+++ login (1).php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost"; // Host name 
+$db_username = "id21939450_felipe"; // Mysql username 
+$db_password = "Fp131921*"; // Mysql password 
+$db_name = "id21939450_jogostmfa"; // Database name 
+$db_table = "score_novo"; // Table name
+
+?>
